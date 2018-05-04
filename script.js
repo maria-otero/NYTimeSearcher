07ffab8eaf80babbebaaa4fe0a1e978d7ffa72ff
Jesse <laethaka@gmail.com>
2018-05-04 15:04:21 -0700

    functional but no links

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-$('#formSearch').on('click', function () {
+// console.log('hi')
+
+$('#formSearch').on('click', function (event) {
+    event.preventDefault();
+    $('#articleField').empty();
+
     //PULLING FORM DATA FROM USER
     var searchInput = $('#termInput').val();
     var numInput = $('#numInput').val();
@@ -9,9 +14,14 @@ $('#formSearch').on('click', function () {
     // searchInput = 'Trump';
     // startInput = 20030101;
     // endInput = 20040101;
+
+    //CONVERTING YEAR TO YYYYMMDD
+    startInput = startInput + '0101';
+    endInput = endInput + '1231';
     
     //BUILD SEARCH URL
     var searchURL = `http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=769ae556259949f9bb72bf4556f8d25c&q=${searchInput}&begin_date=${startInput}&end_date=${endInput}`
+
     $.ajax ({
        url: searchURL,
        method: 'GET'
@@ -23,17 +33,17 @@ $('#formSearch').on('click', function () {
 
             //APPENDING NUMBER
             var articleNum = $('<h3>');
-            articleNum.text(i+1).newClass('article-number');
+            articleNum.text(i+1).addClass('article-number');
             newDiv.append(articleNum);
 
             //APPENDING HEADLINE
             var newHeadline = $('<h3>');
-            newHeadline.text(response.response.docs[i].headline.main).newClass('article-headline');
+            newHeadline.text(response.response.docs[i].headline.main).addClass('article-headline');
             newDiv.append(newHeadline);
 
             //APPENDING SOURCE
             var newSource = $('<p>');
-            newSource.text("By " + response.response.docs[i].source).newClass('article-source');
+            newSource.text("By " + response.response.docs[i].source).addClass('article-source');
             newDiv.append(newSource);
 
             $('#articleField').prepend(newDiv);
